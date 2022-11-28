05ab015b7a2e493c18ee2318bdecec5e62a3e252
VirtualFrog <kek>
2022-11-29 00:19:30 +0200

    Make computer choose an  option

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ function getRandomNumber() {
   return Math.floor(Math.random() * 3) + 1;
 }
 
+// Randomly chose between 3 options
+function getComputerChoice() {
+  let num = getRandomNumber();
+  let choice = '';
+  switch (num) {
+    case 1:
+      choice = 'Rock';
+      break;
+    
+    case 2:
+      choice = 'Paper';
+      break;
+
+    case 3: 
+      choice = 'Scissors';
+      break;
+  }
+  return choice;
+}
